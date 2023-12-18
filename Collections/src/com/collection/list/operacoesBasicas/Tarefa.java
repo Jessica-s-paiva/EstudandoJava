c968df4ab05f7171d6ebc383538133faf2d3f92c
Jessica-s-Paiva <jessicapaivamvp@gmail.com>
2023-12-18 14:02:57 -0300

    atualizando

---
+++ Collections/src/com/collection/list/operacoesBasicas/Tarefa.java
@@ -0,0 +1,17 @@
+package com.collection.list.operacoesBasicas;
+
+public class Tarefa {
+
+	//atributo
+	private String descricao;
+
+	public Tarefa(String descricao) {
+		super();
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+}
